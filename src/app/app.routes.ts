f0c53355e9760a66f2e57a1be46135812c71848a
Alejandro Díaz <victor.diaz.jauregui@mi.unc.edu.ar>
2024-03-29 20:35:15 -0300

    rutas base definidas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,30 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import  { LayoutComponent } from '@components/layout/layout.component';
+
+export const routes: Routes = [
+  {
+    path: '',
+    component: LayoutComponent,
+    children: [
+      path: '',
+      loadComponent: () => import('@pages/home/home.module')
+    ],
+    {
+      path: 'sign-in',
+      loadComponent: () => import('@pages/sign-in/sign-in.module')
+    },
+    {
+      path: 'sign-up',
+      loadComponent: () => import('@pages/sign-up/sign-up.module')
+    },
+    {
+      path: 'release/:id',
+      loadComponent: () => import('@pages/release/release.module')
+    }
+  },
+  {
+    path: '**',
+    loadComponent: () => import('@pages/not-found/not-found.module')
+  }
+];
