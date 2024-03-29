4e1efd2bf5189c786d26abcd46b56a5aa5765e84
Alejandro Díaz <victor.diaz.jauregui@mi.unc.edu.ar>
2024-03-29 20:40:11 -0300

    layout

--- src/app/components/layout/layout.component.ts
+++ src/app/components/layout/layout.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+import { HeaderComponent } from '@components/header/header.component';
 
 @Component({
   selector: 'app-layout',
   standalone: true,
-  imports: [],
-  templateUrl: './layout.component.html',
-  styleUrl: './layout.component.css'
+  imports: [RouterModule, HeaderComponent],
+  templateUrl: './layout.component.html'
 })
 export class LayoutComponent {
 
