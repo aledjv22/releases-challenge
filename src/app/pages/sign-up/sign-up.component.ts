1dd1665d865bde96ee836751b08c5790139de6f4
Alejandro Díaz <victor.diaz.jauregui@mi.unc.edu.ar>
2024-03-30 19:56:34 -0300

    Actualizado el sistema de sign-up

--- src/app/pages/sign-up/sign-up.component.ts
+++ src/app/pages/sign-up/sign-up.component.ts
@@ -1,16 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { RouterLinkWithHref } from '@angular/router';
+
+import { UserService } from '@services/user.service';
 
 @Component({
   selector: 'app-sign-up',
   standalone: true,
-  imports: [RouterLinkWithHref],
+  imports: [RouterLinkWithHref, ReactiveFormsModule],
   templateUrl: './sign-up.component.html'
 })
-export default class SignUpComponent {
+export default class SignUpComponent implements OnInit{
   readonly stylesInput = 'bg-transparent border-2 border-[#FD7658] outline-[#FFA07A] rounded-lg w-full px-2 py-1 mb-2';
 
   readonly stylesButton = `bg-gradient-to-r from-[#FD7658] to-[#FFA07A] 
   hover:bg-gradient-to-r hover:to-[#FD7658] hover:from-[#FFA07A] 
   rounded-lg w-full mt-3 py-3`;
+
+  formReg: FormGroup;
+
+  isRegistered = signal(false);
+
+  constructor(
+    public userService: UserService,
+  ) {
+    this.formReg = new FormGroup({
+      email: new FormControl(),
+      password: new FormControl()
+    });
+  }
+
+  ngOnInit(): void {
+  }
+
+  onSubmit() {
+    this.userService.register(this.formReg.value)
+      .then(response => {
+        this.isRegistered.set(true);
+        console.log(response);
+      })
+      .catch(error => console.log(error));
+  }
 }
