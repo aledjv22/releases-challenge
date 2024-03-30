47a53eb7b90c40c173b58c16fe8fad4956afe2f1
Alejandro Díaz <victor.diaz.jauregui@mi.unc.edu.ar>
2024-03-30 09:21:13 -0300

    Maquetado del sign-in

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-
 import { RouterLinkWithHref } from '@angular/router';
 
 @Component({
--- src/app/pages/sign-in/sign-in.component.ts
+++ src/app/pages/sign-in/sign-in.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
+import { RouterLinkWithHref } from '@angular/router';
 
 @Component({
   selector: 'app-sign-in',
   standalone: true,
-  imports: [],
-  templateUrl: './sign-in.component.html',
-  styleUrl: './sign-in.component.css'
+  imports: [RouterLinkWithHref],
+  templateUrl: './sign-in.component.html'
 })
-export class SignInComponent {
+export default class SignInComponent {
+  readonly stylesInput = 'bg-transparent border-2 border-[#FD7658] outline-[#FFA07A] rounded-lg w-full px-2 py-1 mb-2';
 
+  readonly stylesButton = `bg-gradient-to-r from-[#FD7658] to-[#FFA07A] 
+  hover:bg-gradient-to-r hover:to-[#FD7658] hover:from-[#FFA07A] 
+  rounded-lg w-[300px] mb-4 py-3`;
 }
