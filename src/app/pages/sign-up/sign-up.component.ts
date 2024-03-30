54d1cdf997e4c1864bcd1219ba99e0e0dc3ce3f0
Alejandro Díaz <victor.diaz.jauregui@mi.unc.edu.ar>
2024-03-30 09:44:45 -0300

    Maquetado del sign-up

--- src/app/pages/sign-up/sign-up.component.ts
+++ src/app/pages/sign-up/sign-up.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
+import { RouterLinkWithHref } from '@angular/router';
 
 @Component({
   selector: 'app-sign-up',
   standalone: true,
-  imports: [],
-  templateUrl: './sign-up.component.html',
-  styleUrl: './sign-up.component.css'
+  imports: [RouterLinkWithHref],
+  templateUrl: './sign-up.component.html'
 })
-export class SignUpComponent {
+export default class SignUpComponent {
+  readonly stylesInput = 'bg-transparent border-2 border-[#FD7658] outline-[#FFA07A] rounded-lg w-full px-2 py-1 mb-2';
 
+  readonly stylesButton = `bg-gradient-to-r from-[#FD7658] to-[#FFA07A] 
+  hover:bg-gradient-to-r hover:to-[#FD7658] hover:from-[#FFA07A] 
+  rounded-lg w-full mt-3 py-3`;
 }
