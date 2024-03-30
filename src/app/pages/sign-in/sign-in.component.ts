de35256bf70c65fc6b123503837bc69b80ec15a0
Alejandro Díaz <victor.diaz.jauregui@mi.unc.edu.ar>
2024-03-30 19:44:10 -0300

    Actualizado el sistema de sign-in

--- src/app/pages/sign-in/sign-in.component.ts
+++ src/app/pages/sign-in/sign-in.component.ts
@@ -1,16 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { RouterLinkWithHref } from '@angular/router';
+
+import { UserService } from '@services/user.service';
 
 @Component({
   selector: 'app-sign-in',
   standalone: true,
-  imports: [RouterLinkWithHref],
+  imports: [RouterLinkWithHref, ReactiveFormsModule],
   templateUrl: './sign-in.component.html'
 })
-export default class SignInComponent {
+export default class SignInComponent implements OnInit {
   readonly stylesInput = 'bg-transparent border-2 border-[#FD7658] outline-[#FFA07A] rounded-lg w-full px-2 py-1 mb-2';
 
   readonly stylesButton = `bg-gradient-to-r from-[#FD7658] to-[#FFA07A] 
   hover:bg-gradient-to-r hover:to-[#FD7658] hover:from-[#FFA07A] 
   rounded-lg w-full mb-4 py-3`;
+
+  formLogin: FormGroup;
+
+  constructor(
+    public userService: UserService
+  ) {
+    this.formLogin = new FormGroup({
+      email: new FormControl(),
+      password: new FormControl()
+    })
+  }
+
+  ngOnInit(): void {
+  }
+
+  onSubmit() {
+    this.userService.login(this.formLogin.value)
+      .then(response => {
+        this.userService.isLogged.set(true);
+        this.userService.user.set({ email: response.user.email ?? '', password: ''});
+      })
+      .catch(error => console.log(error));
+  }
+
+  onClick() {
+    this.userService.loginWithGoogle()
+      .then(response => {
+        this.userService.isLogged.set(true);
+        this.userService.user.set({ email: response.user.email ?? '', password: ''});
+      })
+      .catch(error => console.log(error))
+  }
 }
