5aa4476b991f004e394fd4e88f51c00df46f6531
Alejandro Díaz <victor.diaz.jauregui@mi.unc.edu.ar>
2024-03-30 10:09:38 -0300

    Maquetado del not-found

--- src/app/pages/not-found/not-found.component.ts
+++ src/app/pages/not-found/not-found.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
+import { RouterLinkWithHref } from '@angular/router';
 
 @Component({
   selector: 'app-not-found',
   standalone: true,
-  imports: [],
-  templateUrl: './not-found.component.html',
-  styleUrl: './not-found.component.css'
+  imports: [RouterLinkWithHref],
+  templateUrl: './not-found.component.html'
 })
-export class NotFoundComponent {
-
+export default class NotFoundComponent {
+  readonly stylesButton = `bg-gradient-to-r from-[#FD7658] to-[#FFA07A] 
+  hover:bg-gradient-to-r hover:to-[#FD7658] hover:from-[#FFA07A] 
+  rounded-lg w-80 mb-4 py-3 text-white font-semibold`;
 }
