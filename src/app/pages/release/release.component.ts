802d312571b960bb7087943031cbee3f4142bfff
Alejandro Díaz <victor.diaz.jauregui@mi.unc.edu.ar>
2024-03-31 12:14:08 -0300

    Botones de volver y eliminar para el release

--- src/app/pages/release/release.component.ts
+++ src/app/pages/release/release.component.ts
@@ -1,13 +1,15 @@
-import { Component, Input, inject, signal } from '@angular/core';
+import { Component, Input, signal } from '@angular/core';
 import { DatePipe } from '@angular/common';
+import { RouterLinkWithHref } from '@angular/router';
 
 import { Release } from '@models/release.model';
 import { ReleasesService } from '@services/releases.service';
+import { UserService } from '@services/user.service';
 
 @Component({
   selector: 'app-release',
   standalone: true,
-  imports: [DatePipe],
+  imports: [RouterLinkWithHref, DatePipe],
   templateUrl: './release.component.html'
 })
 export default class ReleaseComponent {
@@ -16,13 +18,22 @@ export default class ReleaseComponent {
 
   release = signal<Release | null>(null);
 
-  constructor(private releaseService: ReleasesService) {}
+  constructor(
+    private releaseService: ReleasesService,
+    public userService: UserService
+  ) {}
 
   ngOnInit() {
     if (this.id) {
       this.releaseService.getReleaseById(this.id)
         .then(release => this.release.set(release))
         .catch(error => console.log(error));
+    }
+  }
+
+  async deleteRelease() {
+    if (this.release()?.id) {
+      const response = await this.releaseService.deleteRelease(this.release()!);
     }
   }
 }
