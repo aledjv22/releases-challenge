6dbf28b391dfb1b3be3b009cce29a1e5991164c3
Alejandro Díaz <victor.diaz.jauregui@mi.unc.edu.ar>
2024-03-30 23:51:28 -0300

    Esquema base del home y boton de crear release

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+import { RouterLinkWithHref } from '@angular/router';
+
+import { MonthListComponent } from '@components/month-list/month-list.component';
+import { TagListComponent } from '@components/tag-list/tag-list.component';
+import { CardComponent } from '@components/card/card.component';
+import { UserService } from '@services/user.service';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
-  templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  imports: [RouterLinkWithHref, MonthListComponent, TagListComponent, CardComponent],
+  templateUrl: './home.component.html'
 })
-export class HomeComponent {
-
+export default class HomeComponent {
+  constructor(public userService: UserService) {}
+  
+  readonly stylesButton = `bg-gradient-to-r from-[#FD7658] to-[#FFA07A]
+  hover:bg-gradient-to-r hover:to-[#FD7658] hover:from-[#FFA07A]
+  rounded-lg w-full mb-4 py-3`;
 }
