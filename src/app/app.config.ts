303d0f0421f28c0c8bacb6261e11360a3cd87638
Alejandro Díaz <victor.diaz.jauregui@mi.unc.edu.ar>
2024-03-30 23:02:58 -0300

    Firestore integrado

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,11 +5,33 @@ import { provideHttpClient } from '@angular/common/http';
 import { routes } from './app.routes';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes, withComponentInputBinding(),
     withPreloading(PreloadAllModules)),
-    provideHttpClient(), importProvidersFrom(provideFirebaseApp(() => initializeApp({"projectId":"releases-app-6eb0f","appId":"1:564594943708:web:5043b7481281954609f619","storageBucket":"releases-app-6eb0f.appspot.com","apiKey":"AIzaSyA0bz7JjoDfHOMEPDBDeyX7o-ogQcXo9AU","authDomain":"releases-app-6eb0f.firebaseapp.com","messagingSenderId":"564594943708","measurementId":"G-L5YE5M2D3B"}))), importProvidersFrom(provideAuth(() => getAuth()))
+    provideHttpClient(), importProvidersFrom(provideFirebaseApp(() => initializeApp({
+      "projectId":"releases-app-6eb0f",
+      "appId":"1:564594943708:web:5043b7481281954609f619",
+      "storageBucket":"releases-app-6eb0f.appspot.com",
+      "apiKey":"AIzaSyA0bz7JjoDfHOMEPDBDeyX7o-ogQcXo9AU",
+      "authDomain":"releases-app-6eb0f.firebaseapp.com",
+      "messagingSenderId":"564594943708",
+      "measurementId":"G-L5YE5M2D3B"}))), 
+      importProvidersFrom(provideAuth(() => getAuth())),
+      importProvidersFrom(provideFirebaseApp(() => 
+      initializeApp({
+        "projectId":"releases-app-6eb0f",
+        "appId":"1:564594943708:web:5043b7481281954609f619",
+        "storageBucket":"releases-app-6eb0f.appspot.com",
+        "apiKey":"AIzaSyA0bz7JjoDfHOMEPDBDeyX7o-ogQcXo9AU",
+        "authDomain":"releases-app-6eb0f.firebaseapp.com",
+        "messagingSenderId":"564594943708",
+        "measurementId":"G-L5YE5M2D3B"
+      }))), 
+      importProvidersFrom(provideAuth(() => getAuth())),
+       importProvidersFrom(provideFirestore(() => 
+       getFirestore()))
   ]
 };
