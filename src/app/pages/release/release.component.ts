59cc23a84c040f948b04837938b3663b3fb58b45
Alejandro Díaz <victor.diaz.jauregui@mi.unc.edu.ar>
2024-03-31 11:37:52 -0300

    Pág. release/:id con obtención de valores

--- src/app/pages/release/release.component.ts
+++ src/app/pages/release/release.component.ts
@@ -1,11 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, Input, inject, signal } from '@angular/core';
+import { DatePipe } from '@angular/common';
+
+import { Release } from '@models/release.model';
+import { ReleasesService } from '@services/releases.service';
 
 @Component({
   selector: 'app-release',
   standalone: true,
-  imports: [],
+  imports: [DatePipe],
   templateUrl: './release.component.html'
 })
 export default class ReleaseComponent {
 
+  @Input() id?: string;
+
+  release = signal<Release | null>(null);
+
+  constructor(private releaseService: ReleasesService) {}
+
+  ngOnInit() {
+    if (this.id) {
+      this.releaseService.getReleaseById(this.id)
+        .then(release => this.release.set(release))
+        .catch(error => console.log(error));
+    }
+  }
 }
