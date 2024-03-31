6c347e37672a75d667e419bd2cdf94eabb38c27d
Alejandro Díaz <victor.diaz.jauregui@mi.unc.edu.ar>
2024-03-31 14:54:12 -0300

    Muestra de realeses y carga del mismo en home

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,11 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { DatePipe } from '@angular/common';
+import { RouterLinkWithHref } from '@angular/router';
+
+import { Release } from '@models/release.model';
 
 @Component({
   selector: 'app-card',
   standalone: true,
-  imports: [],
+  imports: [RouterLinkWithHref, DatePipe],
   templateUrl: './card.component.html'
 })
 export class CardComponent {
-
+  @Input({required: true}) release!: Release;
 }
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, Input, inject, SimpleChanges, signal } from '@angular/core';
 import { RouterLinkWithHref } from '@angular/router';
 
 import { MonthListComponent } from '@components/month-list/month-list.component';
 import { TagListComponent } from '@components/tag-list/tag-list.component';
 import { CardComponent } from '@components/card/card.component';
 import { UserService } from '@services/user.service';
+import { Release } from '@models/release.model';
+import { ReleasesService } from '@services/releases.service';
 
 @Component({
   selector: 'app-home',
@@ -13,9 +15,40 @@ import { UserService } from '@services/user.service';
   templateUrl: './home.component.html'
 })
 export default class HomeComponent {
-  constructor(public userService: UserService) {}
+  constructor(
+    public userService: UserService,
+  ) {}
   
   readonly stylesButton = `bg-gradient-to-r from-[#FD7658] to-[#FFA07A]
   hover:bg-gradient-to-r hover:to-[#FD7658] hover:from-[#FFA07A]
-  rounded-lg w-full mb-4 py-3`;
+  rounded-lg w-full mb-4 py-3`
+
+  releases = signal<Release[]>([]);
+  tags = signal<string[]>([]);
+  months = signal<string[]>([]);
+
+  private releasesService = inject(ReleasesService);
+
+  @Input() tag?: string;
+  @Input() month?: string;
+
+  ngOnInit() {
+    this.getReleases();
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    this.getReleases();
+  }
+
+  private getReleases() {
+    this.releasesService.getReleases()
+    .subscribe({
+      next: (releases) => {
+        this.releases.set(releases);
+      },
+      error: () => {
+        console.log('Error')
+      }
+    });
+  }
 }
