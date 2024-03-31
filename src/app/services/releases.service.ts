bea7131b0c23e340548a2db6bdcf4e441f2f9476
Alejandro Díaz <victor.diaz.jauregui@mi.unc.edu.ar>
2024-03-31 09:12:21 -0300

    Integrado el getReleaseById

--- src/app/services/releases.service.ts
+++ src/app/services/releases.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@angular/core';
 import { Firestore, collection, addDoc, collectionData, 
-  doc, deleteDoc } from '@angular/fire/firestore';
+  doc, deleteDoc, getDoc } from '@angular/fire/firestore';
 import { Observable } from 'rxjs';
-
+import { Router } from '@angular/router';
 import { Release } from '@models/release.model';
 
 @Injectable({
@@ -10,11 +10,26 @@ import { Release } from '@models/release.model';
 })
 export class ReleasesService {
 
-  constructor(private firestore: Firestore) { }
+  constructor(
+    private firestore: Firestore,
+    private router: Router
+    ) { }
 
   getReleases(): Observable<Release[]> {
     const releaseRef = collection(this.firestore, 'releases');
     return collectionData(releaseRef, { idField: 'id' }) as Observable<Release[]>;
+  }
+
+  async getReleaseById(id: string): Promise<Release> {
+    const releaseDocRef = doc(this.firestore, `releases/${id}`);
+    return getDoc(releaseDocRef).then(docSnap => {
+      if (docSnap.exists()) {
+        this.router.navigateByUrl(`/release/${id}`);
+        return { id, ...docSnap.data() } as Release;
+      } else {
+        throw new Error('El release no existe.');
+      }
+    });
   }
 
   postRelease(release: Release) {
