abfc267d083cbb941a116e995af45d1f9f7cb844
Alejandro Díaz <victor.diaz.jauregui@mi.unc.edu.ar>
2024-03-30 21:11:03 -0300

    Firebase autenticación

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,15 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { PreloadAllModules, provideRouter, withComponentInputBinding, withPreloading } from '@angular/router';
 import { provideHttpClient } from '@angular/common/http';
 
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes, withComponentInputBinding(),
     withPreloading(PreloadAllModules)),
-    provideHttpClient()
+    provideHttpClient(), importProvidersFrom(provideFirebaseApp(() => initializeApp({"projectId":"releases-app-6eb0f","appId":"1:564594943708:web:5043b7481281954609f619","storageBucket":"releases-app-6eb0f.appspot.com","apiKey":"AIzaSyA0bz7JjoDfHOMEPDBDeyX7o-ogQcXo9AU","authDomain":"releases-app-6eb0f.firebaseapp.com","messagingSenderId":"564594943708","measurementId":"G-L5YE5M2D3B"}))), importProvidersFrom(provideAuth(() => getAuth()))
   ]
 };
