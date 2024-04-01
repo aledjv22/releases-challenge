6a6ddb8a0d9935baf0ad279d5cccb1aba17b8f2c
Alejandro Díaz <victor.diaz.jauregui@mi.unc.edu.ar>
2024-03-31 21:05:53 -0300

    Integración del footer

--- src/app/components/layout/layout.component.ts
+++ src/app/components/layout/layout.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 import { HeaderComponent } from '@components/header/header.component';
+import { FooterComponent } from '@components/footer/footer.component';
 
 @Component({
   selector: 'app-layout',
   standalone: true,
-  imports: [RouterModule, HeaderComponent],
+  imports: [RouterModule, HeaderComponent, FooterComponent],
   templateUrl: './layout.component.html'
 })
 export class LayoutComponent {
